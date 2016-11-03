71e31b49441ca5000e67b781daa8954210647fc0
Kenneth <kennethlumalicay@gmail.com>
2016-11-02 21:24:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 $(document).ready(function () {
 	$("#aboutme-btn").on("click", function() {
-		$("#right").fadeTo(200, 0.01, function() {
-			$("#left").fadeTo(200, 1).removeClass("shrink").addClass("wrapper overflow views large-view");
-			$("#middle").removeClass("place-mid place-left").addClass("place-right");
-			$("#right").fadeTo(400, 0).removeClass("wrapper overflow views normal-view large-view").addClass("shrink");
+		$("#right").fadeTo(200, 0, function() {
+			$("#right").addClass("shrink").removeClass("wrapper overflow views normal-view large-view").queue(function() {
+				$("#middle").addClass("place-right").removeClass("place-mid place-left");
+				$("#left").clearQueue().fadeTo(200, 1).addClass("wrapper overflow views large-view").removeClass("shrink");
+				$("body").css("background-color", "#e6e6e6");
+				$("#logo").css("filter","invert(100%)");
+				$("#aboutme-btn").css("opacity", "0").prop("disabled", true);
+				$("#works-btn").css({"opacity":"1", "filter":"invert(100%)"}).prop("disabled", false);
+			});
 		});
 	});
 	$("#works-btn").on("click", function() {
-		$("#left").fadeTo(200, 0.01, function() {
-			$("#right").fadeTo(200, 1).removeClass("shrink").addClass("wrapper overflow views large-view");
-			$("#middle").removeClass("place-mid place-right").addClass("place-left");
-			$("#left").fadeTo(400, 0).removeClass("wrapper overflow views normal-view large-view").addClass("shrink");
+		$("#left").fadeTo(200, 0, function() {
+			$("#left").addClass("shrink").removeClass("wrapper overflow views normal-view large-view").queue(function() {
+				$("#middle").addClass("place-left").removeClass("place-mid place-right");
+				$("#right").clearQueue().fadeTo(200, 1).addClass("wrapper overflow views large-view").removeClass("shrink");
+				$("body").css("background-color", "#e6e6e6");
+				$("#logo").css("filter","invert(100%)");
+				$("#works-btn").css("opacity", "0").prop("disabled", true);
+				$("#aboutme-btn").css({"opacity":"1", "filter":"invert(100%)"}).prop("disabled", false);
+			});
 		});
 	});
 	$("#logo-btn").on("click", function() {
-		$("#left").fadeTo(200, 1).removeClass("shrink large-view").addClass("wrapper overflow views normal-view");
-		$("#right").fadeTo(200, 1).removeClass("shrink large-view").addClass("wrapper overflow views normal-view");
-		$("#middle").removeClass("place-right place-left").addClass("place-mid");
+		$("#left").clearQueue().fadeTo(200, 1).addClass("wrapper overflow views normal-view").removeClass("shrink large-view");
+		$("#right").clearQueue().fadeTo(200, 1).addClass("wrapper overflow views normal-view").removeClass("shrink large-view");
+		$("#middle").addClass("place-mid").removeClass("place-right place-left");
+		$("body").css("background-color", "black");
+		$("#logo").css("filter","none");
+		$("#works-btn").css({"opacity":"1", "filter":"none"}).prop("disabled", false);
+		$("#aboutme-btn").css({"opacity":"1", "filter":"none"}).prop("disabled", false);
 	});
 });
