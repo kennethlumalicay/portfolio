da6dad92cec2cd47f4140e882be1976a559cdacb
Kenneth <kennethlumalicay@gmail.com>
2016-11-02 23:16:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
 $(document).ready(function () {
+	function welcome() {
+		var wctext = 'System.out.print("Welcome!");';
+		$("#wctext").html(wctext).css("opacity","1");
+	};
+	welcome();
+	setTimeout(function() {
+		$("#welcome").css({
+			"opacity":"0",
+			"top":"50%",
+			"bottom":"50%",
+			"left":"50%",
+			"right":"50%",
+		});
+	}, 2500);
+	setTimeout(function() {
+		$("#welcome").css("display","none");
+	}, 3500);
 	$("#aboutme-btn").on("click", function() {
 		$("#right").fadeTo(200, 0, function() {
-			$("#right").addClass("shrink").removeClass("wrapper overflow views normal-view large-view").queue(function() {
-				$("#middle").addClass("place-right").removeClass("place-mid place-left");
-				$("#left").clearQueue().fadeTo(200, 1).addClass("wrapper overflow views large-view").removeClass("shrink");
-				$("body").css("background-color", "#e6e6e6");
-				$("#logo").css("filter","invert(100%)");
-				$("#aboutme-btn").css("opacity", "0").prop("disabled", true);
-				$("#works-btn").css({"opacity":"1", "filter":"invert(100%)"}).prop("disabled", false);
-			});
+			$("#right").addClass("shrink").removeClass("overflow rewidth normal-view large-view");
+			$("#middle").addClass("place-right").removeClass("place-mid place-left");
+			$("#left").fadeTo(200, 1).addClass("overflow rewidth large-view").removeClass("shrink");
 		});
+		$("body").css("background-color", "#e6e6e6");
+		$("#logo").css("filter","invert(100%)");
+		$("#aboutme-btn").css("opacity", "0").prop("disabled", true);
+		$("#works-btn").css({"opacity":"1", "filter":"invert(100%)"}).prop("disabled", false);
 	});
 	$("#works-btn").on("click", function() {
 		$("#left").fadeTo(200, 0, function() {
-			$("#left").addClass("shrink").removeClass("wrapper overflow views normal-view large-view").queue(function() {
-				$("#middle").addClass("place-left").removeClass("place-mid place-right");
-				$("#right").clearQueue().fadeTo(200, 1).addClass("wrapper overflow views large-view").removeClass("shrink");
-				$("body").css("background-color", "#e6e6e6");
-				$("#logo").css("filter","invert(100%)");
-				$("#works-btn").css("opacity", "0").prop("disabled", true);
-				$("#aboutme-btn").css({"opacity":"1", "filter":"invert(100%)"}).prop("disabled", false);
-			});
+			$("#left").addClass("shrink").removeClass("overflow rewidth normal-view large-view");
+			$("#middle").addClass("place-left").removeClass("place-mid place-right");
+			$("#right").fadeTo(200, 1).addClass("overflow rewidth large-view").removeClass("shrink");
 		});
+		$("body").css("background-color", "#e6e6e6");
+		$("#logo").css("filter","invert(100%)");
+		$("#works-btn").css("opacity", "0").prop("disabled", true);
+		$("#aboutme-btn").css({"opacity":"1", "filter":"invert(100%)"}).prop("disabled", false);
 	});
 	$("#logo-btn").on("click", function() {
-		$("#left").clearQueue().fadeTo(200, 1).addClass("wrapper overflow views normal-view").removeClass("shrink large-view");
-		$("#right").clearQueue().fadeTo(200, 1).addClass("wrapper overflow views normal-view").removeClass("shrink large-view");
+		$("#left").fadeTo(200, 1).addClass("overflow rewidth normal-view").removeClass("shrink large-view");
+		$("#right").fadeTo(200, 1).addClass("overflow rewidth normal-view").removeClass("shrink large-view");
 		$("#middle").addClass("place-mid").removeClass("place-right place-left");
 		$("body").css("background-color", "black");
 		$("#logo").css("filter","none");
